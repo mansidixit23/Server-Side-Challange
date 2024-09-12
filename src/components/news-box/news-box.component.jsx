8c287f9967158733f55cf29b9a6712215f3764e9
Premchand <premchandmali001@gmail.com>
2024-09-12 10:18:40 +0530

    new changes

--- src/components/news-box/news-box.component.jsx
+++ src/components/news-box/news-box.component.jsx
@@ -1,16 +1,21 @@
-import React, { useContext, useState } from 'react';
+import React, { useContext, useState, useEffect, useRef } from 'react';
 import './news-box.style.scss';
 import { formatDate } from '../../lib/utils/utils';
 import BookmarkBorderOutlinedIcon from '@mui/icons-material/BookmarkBorderOutlined';
 import BookmarkIcon from '@mui/icons-material/Bookmark';
+import ShareIcon from '@mui/icons-material/Share';
+import TwitterIcon from '@mui/icons-material/Twitter';  // Import Twitter icon
+import FacebookIcon from '@mui/icons-material/Facebook';  // Import Facebook icon
+import WhatsAppIcon from '@mui/icons-material/WhatsApp';  // Import WhatsApp icon
 import { UserContext } from '../../context/user-context';
 import { addArticleToBookmarks, removeArticleFromBookmarks } from '../../lib/utils/firebase.utils';
 import { useNavigate } from 'react-router-dom';
 
 function NewsBox({ item }) {
     const { currentUser, userBookmarks } = useContext(UserContext);
-
     const [imageError, setImageError] = useState(false);
+    const [isShareMenuVisible, setIsShareMenuVisible] = useState(false);
+    const shareMenuRef = useRef(null);
 
     const navigate = useNavigate();
 
@@ -22,7 +27,7 @@ function NewsBox({ item }) {
             },
             currentUser?.uid
         );
-    }
+    };
 
     const handleRemoveArticleFromBookmarks = async () => {
         await removeArticleFromBookmarks(
@@ -31,71 +36,107 @@ function NewsBox({ item }) {
                 ...(item.content ? { content: item.content.split('[')[0] } : {})
             },
             currentUser?.uid
-        )
-    }
+        );
+    };
 
     const goToArticlePage = () => {
-        const title = item.title.split('?').join('').split('%').join('');
+        const title = item.title.replace(/[^a-zA-Z0-9]/g, '');
         navigate(`/article/${title}`);
-    }
+    };
+
+    const toggleShareMenu = () => {
+        setIsShareMenuVisible(!isShareMenuVisible);
+    };
+
+    const handleClickOutside = (event) => {
+        if (shareMenuRef.current && !shareMenuRef.current.contains(event.target)) {
+            setIsShareMenuVisible(false);
+        }
+    };
+
+    useEffect(() => {
+        // Add event listener for clicks outside the share menu
+        document.addEventListener('mousedown', handleClickOutside);
+        return () => {
+            // Clean up the event listener on component unmount
+            document.removeEventListener('mousedown', handleClickOutside);
+        };
+    }, []);
 
     return (
-        <div className='news-box'>
-            <div className='image-container'>
-                {
-                    !imageError ? 
-                    <img 
-                        src={item?.urlToImage} 
-                        alt="" 
-                        onError={() => setImageError(true)}  
-                    /> : 
-                    <img 
-                        src="https://resource.rentcafe.com/image/upload/q_auto,f_auto,c_limit,w_576/s3/2/50552/image%20not%20available(34).jpg" 
-                        alt="" 
+        <div className="news-box">
+            <div className="image-container">
+                {!imageError ? (
+                    <img
+                        src={item?.urlToImage}
+                        alt=""
+                        onError={() => setImageError(true)}
                     />
-                }
+                ) : (
+                    <img src="" alt="" />
+                )}
             </div>
 
             <div className="news-content">
                 <h1>{item.title}</h1>
+                <p style={{ fontWeight: 200 }}>{item.description}</p>
 
-                <h2>{item.description}</h2>
-                
                 <div className="news-meta">
                     <ul>
-                        {item.author && <li className='author'>Curated By: <span>{item.author}</span></li>}
-                        <li><span>{item.source.name}</span></li>
-                        <li>Last Updated: <span className='date'>{formatDate(item.publishedAt)}</span></li>
+                        {item.author && (
+                            <li className="author">
+                                Curated By: <span>{item.author}</span>
+                            </li>
+                        )}
+                        <li>
+                            <span>{item.source.name}</span>
+                        </li>
+                        <li>
+                            Last Updated: <span className="date">{formatDate(item.publishedAt)}</span>
+                        </li>
                         <li onClick={goToArticlePage}>Read More</li>
                     </ul>
 
                     <div className="bookmark-container">
-                        {userBookmarks.some(obj => obj.title === item.title) ? 
-                            <BookmarkIcon 
-                                className='marked'
-                                onClick={handleRemoveArticleFromBookmarks} 
-                            /> : 
-                            <BookmarkBorderOutlinedIcon 
-                                onClick={handleAddArticleToBookmarks} 
+                        {userBookmarks.some(obj => obj.title === item.title) ? (
+                            <BookmarkIcon
+                                className="marked"
+                                onClick={handleRemoveArticleFromBookmarks}
                             />
-                        }
+                        ) : (
+                            <BookmarkBorderOutlinedIcon
+                                onClick={handleAddArticleToBookmarks}
+                            />
+                        )}
+                    </div>
+
+                    <div className="share-container">
+                        <ShareIcon onClick={toggleShareMenu} />
+                        {isShareMenuVisible && (
+                            <div className="share-menu" ref={shareMenuRef}>
+                                <a href={`https://twitter.com/share?url=${item.url}`} target="_blank" rel="noopener noreferrer">
+                                    <TwitterIcon className="social-icon" />
+                                    Twitter
+                                </a>
+                                <a href={`https://www.facebook.com/sharer/sharer.php?u=${item.url}`} target="_blank" rel="noopener noreferrer">
+                                    <FacebookIcon className="social-icon" />
+                                    Facebook
+                                </a>
+                                <a
+                                    href={`https://api.whatsapp.com/send?text=${encodeURIComponent(`${item.title} - ${item.url}`)}`}
+                                    target="_blank"
+                                    rel="noopener noreferrer"
+                                >
+                                    <WhatsAppIcon className="social-icon" />
+                                    WhatsApp
+                                </a>
+                            </div>
+                        )}
                     </div>
                 </div>
             </div>
-
-            <div className="bookmark-container">
-                {userBookmarks.some(obj => obj.title === item.title) ? 
-                    <BookmarkIcon 
-                        className='marked'
-                        onClick={handleRemoveArticleFromBookmarks} 
-                    /> : 
-                    <BookmarkBorderOutlinedIcon 
-                        onClick={handleAddArticleToBookmarks} 
-                    />
-                }
-            </div>
         </div>
-    )
+    );
 }
 
 export default NewsBox;
--- src/lib/config/firebase.js
+++ src/lib/config/firebase.js
@@ -4,12 +4,12 @@ import { GoogleAuthProvider, getAuth } from 'firebase/auth';
 import { getStorage } from "firebase/storage";
 
 const firebaseConfig = {
-    apiKey: process.env.REACT_APP_API_KEY,
-    authDomain: process.env.REACT_APP_AUTH_DOMAIN,
-    projectId: process.env.REACT_APP_PROJECT_ID,
-    storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
-    messagingSenderId: process.env.REACT_APP_SENDER_ID,
-    appId: process.env.REACT_APP_APP_ID
+    apiKey: `AIzaSyCiQ5SGxtTp6iNnkFzD9OC9klOEKvibOPg`,
+    authDomain: `react-news-app-535ba.firebaseapp.com`,
+    projectId: `react-news-app-535ba`,
+    storageBucket: `react-news-app-535ba.appspot.com`,
+    messagingSenderId: `109446647300`,
+    appId: `1:109446647300:web:d44dd1b5b43d268abd2f51`,
 };
 
 const app = initializeApp(firebaseConfig);
